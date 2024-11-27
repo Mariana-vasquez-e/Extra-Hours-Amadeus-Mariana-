9ecdd49f4eeec6050425eba13e64fb8c540f40c3
Mariana-vasquez-e <marianavasquezmultimedia@gmail.com>
2024-11-26 23:23:23 -0500

    organización generate report

--- frontend/src/components/inside/GenerateReport.jsx
+++ frontend/src/components/inside/GenerateReport.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import * as XLSX from "xlsx";
 /*
  * Tipo de Script: Component.
@@ -8,13 +8,35 @@ import * as XLSX from "xlsx";
  * Descripción: Este componente es un boton qu permite generar un reporte en excel con la información generada hasta la fecha al dar click en este.
  */
 const GenerateReport = ({ data }) => {
+    const [extraHours, setExtraHours] = useState([]);
+    console.log("report",data)
+    let newData = data.map((hour) => {
+
+    console.log(hour)
+        return({
+                id: hour.id,
+                comments: hour.comments,
+                employeeId: hour.employee.employeeId,
+                employeeName: hour.employee.employeeName,
+                salary: hour.employee.salary,
+                areaName: hour.employee.area.areaName,
+                jobName: hour.employee.job.jobName ,
+                amountExtraHours: hour.amountExtraHours,
+                startDatetime: hour.startDatetime,
+                endDatetime: hour.endDatetime,
+                totalExtraHour: hour.totalExtraHour,
+                totalPayment: hour.totalPayment,
+                description: hour.extraHourTypeDescription,
+            });
+        })
+    console.log(newData)
     const handleDownload = () => {
         /*
          * Se crea una hoja de calculo con la información recibida "data"
          * luego se crea un libro y se agrega a la hoja de calculo, despues
          * se genera como tal el archivvo de excel y se procede a la descarga
          */
-        const ws = XLSX.utils.json_to_sheet(data);
+        const ws = XLSX.utils.json_to_sheet(extraHours);
         const wb = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(wb, ws, "Horas Extras Ingeniería");
         XLSX.writeFile(wb, "ReporteHorasExtrasIngenieria.xlsx");
